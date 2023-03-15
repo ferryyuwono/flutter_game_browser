2b35274ef689f6a4e1b3fbdff33a09a7e97ec2b4
Ferry Yuwono <ferry.yuwono@gojek.com>
2023-03-15 11:41:18 +0700

    Initial project setup

---
+++ packages/game_browser/android/app/src/main/kotlin/com/ferry/game_browser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ferry.game_browser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/game_browser/lib/src/l10n/generated/game_browser_localizations_en.dart
@@ -0,0 +1,9 @@
+import 'game_browser_localizations.dart';
+
+/// The translations for English (`en`).
+class GameBrowserLocalizationsEn extends GameBrowserLocalizations {
+  GameBrowserLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appBarTitle => 'Playstation 5 Game Browser';
+}
