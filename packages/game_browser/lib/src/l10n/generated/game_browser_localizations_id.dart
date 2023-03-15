5d407910b449e448f3790f3397b9216bca98d95e
Ferry Yuwono <ferry.yuwono@gojek.com>
2023-03-15 11:41:18 +0700

    Initial project setup

---
+++ packages/game_browser/lib/src/l10n/generated/game_browser_localizations_id.dart
@@ -0,0 +1,9 @@
+import 'game_browser_localizations.dart';
+
+/// The translations for Indonesian (`id`).
+class GameBrowserLocalizationsId extends GameBrowserLocalizations {
+  GameBrowserLocalizationsId([String locale = 'id']) : super(locale);
+
+  @override
+  String get appBarTitle => 'Playstation 5 Game Browser';
+}
