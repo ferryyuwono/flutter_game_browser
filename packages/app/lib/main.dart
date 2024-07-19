453069cb283ee883c6a35aebf97879569bc202a7
omnifei <omnifei@gmail.com>
2024-07-19 03:20:55 +0000

    Add support for idx and latest flutter

--- packages/app/lib/main.dart
+++ packages/app/lib/main.dart
@@ -7,9 +7,5 @@ import 'package:initializer/initializer.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppInitializer().init();
-  await runZonedGuarded(_runApp, (error, stack) { });
-}
-
-Future<void> _runApp() async {
   runApp(const GameBrowserApp());
 }
